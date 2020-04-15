bfbc1d86cb598ced92830ff01aebc1aa4ffa1324
Jody Russell <missjodyrussell@gmail.com>
2020-04-15 15:51:07 -0600

    styling updated across pages

--- client/src/pages/Goals.js
+++ client/src/pages/Goals.js
@@ -11,12 +11,12 @@ function Goals() {
     // const [goal, setGoal] = useState([])
     const [targetDate, setTargetDate] = useState([])
     const [formObject, setFormObject] = useState({})
-    const { goals, loadGoals, user} = useContext(userContext)
+    const { goals, loadGoals, user } = useContext(userContext)
     console.log("user and goals", user, goals)
 
-    useEffect(()=>{
+    useEffect(() => {
         loadGoals()
-    },[])
+    }, [])
 
     // Deletes a book from the database with a given id, then reloads books from the db
     function deleteGoal(id) {
@@ -51,9 +51,19 @@ function Goals() {
 
     return (
         <div className="containerGoal page">
+
+            <div className="row">
+                <div className="col s-8 push-s4 parent">
+                    <h5 className="quote" >"Cute Quote Here"<br />--About Goals!</h5>
+                    <img src="./images/hand.png" className="goal-image" />
+                    <h1 className="goal-child">Set a new goal</h1>
+                </div>
+            </div>
+
+
             <div className="row">
                 <div className="col s12">
-                    <h2>Set a new goal</h2>
+                    {/* <h2>Set a new goal</h2> */}
                     <form>
                         <div className="row" >
                             <div className="col s12 m7">
