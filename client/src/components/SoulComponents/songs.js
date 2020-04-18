d5009b501cebaa343f2fcd41cd23bbb7a3514315
inbary-s <inbarsim6@gmail.com>
2020-04-18 10:53:20 -0600

    music test

--- client/src/components/SoulComponents/songs.js
+++ client/src/components/SoulComponents/songs.js
@@ -1,18 +1,18 @@
 export default [
   {
-    url: "./music/129219__juskiddink__quartz-crystal-singing-bowl.wav",
+    url: "./music/129219__juskiddink__quartz-crystal-singing-bowl_01.mp3",
     // title: "Quartz Crystal Singing Bowl by juskiddink",
     title: "Singing Bowl",
     duration: 0
   },
   {
-    url: "./music/244155__metzik__throat-singing.wav",
+    url: "./music/244155__metzik__throat-singing_01.mp3",
     // title: "Throat Singing by Metzik",
     title: "Throat Singing",
     duration: 0
   },
   {
-    url: "./music/447511__andrewkn__dunes.wav",
+    url: "./music/447511__andrewkn__dunes_01.mp3",
     // title: "Dunes by Andrewkn",
     title: "Dunes",
     duration: 0
