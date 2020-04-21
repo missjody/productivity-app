53a2ffdcf61499cd9712b4f199c216950c7e155a
Jody Russell <missjodyrussell@gmail.com>
2020-04-21 17:17:57 -0600

    css updates

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -21,18 +21,19 @@ const HomePage = () => {
     <div className="row" ref={el => app = el}>
       <div ref={el => content = el}>
 
-        <div className="col s-12 m-9">
+        <div className="col s12 m12 push-m2 xl6 center-align" >
           <h4 id="homeText">
             Welcome to Surată! Your new little helper. <br /><br />
         Here you can keep track of your Goals and break each goal down into Tasks.<br /><br />
         Keep track of due dates with your Plan page, a calendar displaying your task todos.<br /><br />
         Don't forget to take care of yourself on the My Soul portion, where you can take a moment to meditate to a desired sound track.<br /><br />
           </h4>
-          <div className="col s-12 m-6">
-            <img src="./images/Surata.png" className="responsive-img" id="homeImage"
-              alt="Image of the Surata app's mascot, Little Sister. She has a high brown bun, large green eyes, blushing cheeks and a sweet blue sweater." />
-          </div >
+        </div>
+        <div className="col s12 m6 xl6" >
+          <img src="./images/Surata.png" className="responsive-img" id="homeImage"
+            alt="Image of the Surata app's mascot, Little Sister. She has a high brown bun, large green eyes, blushing cheeks and a sweet blue sweater." />
         </div >
+        {/* </div > */}
       </div>
     </div>
 
