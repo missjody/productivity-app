9b878d0980eb36367d47b81dd2bf89ac495da6f5
Jody Russell <missjodyrussell@gmail.com>
2020-06-03 19:51:07 -0600

    my goals/login/home page images updated

--- client/src/components/SoulComponents/SongSelector.js
+++ client/src/components/SoulComponents/SongSelector.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { Row } from "../Grid";
 import { Transition } from 'react-transition-group';
+import xsmall from "./xsmall.png";
+import small from "./small.png";
+import medium from "./medium.png";
+import large from "./large.png";
+import xlarge from "./xlarge.png";
 
 const duration = 1000;
 
@@ -37,10 +42,20 @@ class SongSelector extends Component {
               ...defaultStyle,
               ...transitionStyles[state]
             }}>
+              <img
+                sizes="(max-width: 100vw) 100vw"
+                srcset={`${xsmall} 190w,
+            ${small} 879w,
+            ${medium} 1308w,
+            ${large} 1703w,
+            ${xlarge} 2600w`}
+                src="yellowmeditate_cxfljk_c_scale,w_2600.png"
+                alt="An image of a womans hands resting on a computer keyboard. The photo is composed of black and yellow lines striped together to render the image." />
               <Row>
                 <div className="col s-8 pull-s1 parent">
                   <h5 className="quote" >"Meditation brings many benefits: It refreshes us, helps us settle into what’s happening now, makes us wiser and gentler, helps us cope in a world that overloads us with information and communication, and more."<br />--Gayathri Sooraj</h5>
-                  <img src="./images/soul1.png" className="soul-image responsive-img" alt="A person stands against a wall in a one legged yoga pose, hands folded together in front of them. Their chin is uplifted, and they look content. The photo is composed of black and teal lines striped together to render the image." />
+
+                  {/* <img src="./images/soul1.png" className="soul-image responsive-img" alt="A person stands against a wall in a one legged yoga pose, hands folded together in front of them. Their chin is uplifted, and they look content. The photo is composed of black and teal lines striped together to render the image." /> */}
                   <h1 className="child">My Soul</h1>
                 </div>
               </Row>
